a5bb114900f3fc681af8d23e92cafb3b3c017303
Terry <terry@3si.me>
2021-01-05 22:27:12 +0100

    Fixes and additions

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,37 @@
+var debug = require('debug')('hello-bb:route')
 var express = require('express');
 var router = express.Router();
 
 
 var mysql = require('mysql')
-var conn = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PW || '',
-  database: process.env.DB || 'mydb'
-})
-conn.connect()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  conn.query('SELECT description from mytable', function(error, results, fields) {
-    if (error) throw error;
-    result = [];
-
-    for(r in results) {
-      result.push(results[r].description)
-    }
-
-    res.send('Got results: ' + result.join(' '));
+  var conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB
   })
+
+  try {
+    conn.query('SELECT description from mytable', function (error, results, fields) {
+      if (error) {
+        res.send('Error executing query to DB, reason: ' + error)
+        console.error(error)
+        return;
+      }
+      result = [];
+
+      for (r in results) {
+        result.push(results[r].description)
+      }
+
+      res.send('Got results: ' + result.join(' '));
+    })
+  } finally {
+    conn.end()
+  }
 });
 
 module.exports = router;
